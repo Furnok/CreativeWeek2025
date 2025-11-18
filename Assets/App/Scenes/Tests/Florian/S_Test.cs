40b733879435a594fcdaf143cb6be012cbc84e5c
MartinsFlorian <f.martins2005.fm@gmail.com>
2025-11-18 15:47:32 +0100

    Test

---
+++ Assets/App/Scenes/Tests/Florian/S_Test.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class S_Test : MonoBehaviour
+{
+    //[Header("Settings")]
+
+    //[Header("References")]
+
+    //[Header("Inputs")]
+
+    //[Header("Outputs")]
+
+    private void Start()
+    {
+        Debug.Log("Test");
+    }
+}
