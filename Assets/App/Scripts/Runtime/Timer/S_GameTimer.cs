8a9819a914bcf552406548d5eb3b242f483489a2
N3crom <mathias.manenq@gmail.com>
2025-11-21 08:33:26 +0100

    feat end time illu miss call what event afterr to restart game/scene and go menu

--- Assets/App/Scripts/Runtime/Timer/S_GameTimer.cs
+++ Assets/App/Scripts/Runtime/Timer/S_GameTimer.cs
@@ -1,12 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class S_GameTimer : MonoBehaviour
 {
-    //[Header("Settings")]
+    [Header("Settings")]
+    [SerializeField] List<S_ClassIllustation> _illuMentalReachZero = new();
 
     [Header("References")]
     [SerializeField] SSO_Game_Settings _gameSettings;
     [SerializeField] RSO_GameTimeRemaining _gameTimeRemaining;
+    [SerializeField] SSO_FadeTime _fadeTimeSso;
 
     [Header("Inputs")]
     [SerializeField] RSE_OnStartGameTimer _onStartGameTimerRse;
@@ -14,6 +17,10 @@ public class S_GameTimer : MonoBehaviour
 
     [Header("Outputs")]
     [SerializeField] RSE_OnGameTimerEnd _onGameTimerEndRse;
+    [SerializeField] RSE_OnIllustration _onIllustrationRse;
+    [SerializeField] RSE_OnFadeOut _onFadeOutRse;
+    [SerializeField] RSE_OnFadeIn _onFadeInRse;
+
 
     private bool _timerRunning = false;
     void OnEnable()
@@ -27,6 +34,11 @@ public class S_GameTimer : MonoBehaviour
         _onResetAfterMentalReachZeroRse.action-= AdvanceTimer;
     }
 
+
+    private void Start()
+    {
+        //StartTimer();  //Testing
+    }
     void StartTimer()
     {
         _gameTimeRemaining.Value = _gameSettings.Value.TimerDuration;
@@ -42,7 +54,7 @@ public class S_GameTimer : MonoBehaviour
             _gameTimeRemaining.Value = 0f;
             _timerRunning = false;
 
-            _onGameTimerEndRse.Call();
+            TriggerGameOverSequence();
         }
     }
 
@@ -57,8 +69,23 @@ public class S_GameTimer : MonoBehaviour
         {
             _gameTimeRemaining.Value = 0f;
             _timerRunning = false;
+            TriggerGameOverSequence();
 
-            _onGameTimerEndRse.Call();
         }
+    }
+
+    void TriggerGameOverSequence()
+    {
+        _onGameTimerEndRse.Call();
+
+        _onFadeOutRse.Call();
+        StartCoroutine(S_Utils.Delay(_fadeTimeSso.Value, () =>
+        {
+            _onIllustrationRse.Call(_illuMentalReachZero);
+            StartCoroutine(S_Utils.DelayRealTime(_illuMentalReachZero[0].time, () =>
+            {
+                // Restart the game/scene or go to main menu
+            }));
+        }));
     }
 }
